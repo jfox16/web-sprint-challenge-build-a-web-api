e4ca431983cc195440cbebec548ceca1f633055d
Jonathan Fox <FoxJonathanP@gmail.com>
2021-04-09 11:58:23 -0700

    Added projects GET by id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -11,4 +11,8 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({ message: err.message }));
 });
 
+router.get('/:id', validateProjectId, (req, res) => {
+  res.status(200).json(req.project);
+});
+
 module.exports = router;
